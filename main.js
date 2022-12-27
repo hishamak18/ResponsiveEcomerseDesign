9bbcdd03b4b678087944d4890836c0d2610ea8d0
hisham ak <hishamak2004@gmail.com>
2022-12-27 19:11:15 +0530

    Made as Responsive with Media Query

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
- 
+const navBar =document.querySelector('#bar')
+const nav =document.querySelector('#navbar')
+const close =document.querySelector('#close')
 
+
+if(navBar){
+    navBar.addEventListener('click',() =>{
+        nav.classList.add('active')
+        console.log('hii hisham')
+    })
+}
+if(close){
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active')
+        console.log('hii hisham')
+    })
+}
